4351d0d1105c2c11d8ed6962828d0892f8c23c0d
Toluwalashe <tee1alive@yahoo.com>
2024-04-17 00:51:36 +0100

    auth controleer updated

--- src/app/controllers/student/authController.js
+++ src/app/controllers/student/authController.js
@@ -1,6 +1,8 @@
-var models = require("../../../models");
+const models = require("../../../models");
 const { jsonS, jsonFailed } = require("../../../utils");
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const config = require("../../../config/jwt");
 
 let controller = {
     login: async (req, res)=> {
@@ -16,7 +18,49 @@ let controller = {
           if (!passwordIsValid){
             return jsonFailed(res, {}, "Invalid Credentials", 400);
           }
-          jsonS(res, 200, "Successfully logged in")
-    }
+          req.session.user = isStudent;
+          const data = controller.getToken(isStudent);
+        return jsonS(res, 200, "Successful", data);
+    },
+    
+    getToken: (user) => {
+      let token = jwt.sign(
+        { id: user.uuid, email: user.email, studentId: user.studentId },
+        config.jwt_secret,
+        {
+          expiresIn: 2630000, // expires in 1 month
+        }
+      );
+      let refreshToken = jwt.sign({ id: user.uuid, studentId: user.studentId }, config.jwt_secret);
+      let data = {
+      token: token,
+      refreshToken: refreshToken,
+      token_type: "jwt",
+      expiresIn: 2630000,
+    };
+    return data;
+    },
+
+    refreshToken: (req, res) => {
+      const { token } = req.body;
+      jwt.verify(token, config.jwt_secret, async (err, decoded) => {
+        if (err) {
+          return jsonFailed(res, {}, "Token Expired", 403);
+        }
+        //
+        let user = await models.Student.findOne({ studentId: decoded.studentId });
+        const data = controller.getToken(user);
+        return jsonS(res, 200, "Successful", data);
+      });
+    },
+
+    logout: (req, res) => {
+      if (req.session.user) {
+        req.session.user = null;
+        //
+        return jsonS(res, null, "logged out successfully");
+      }
+      return jsonS(res, null, "logged out successfully");
+    },
     };
     module.exports = controller;
